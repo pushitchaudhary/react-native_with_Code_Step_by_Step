1c73a2e8671b0395cd656127714d1ee452888f97
pushitchaudhary <pushitcoc@gmail.com>
2024-04-06 16:17:26 +0545

    components

--- App.js
+++ App.js
@@ -1,16 +1,53 @@
 import React from "react";
 
 import { View,Text, Button } from "react-native";
+import CompanyData from "./components/CompanyData";
 
 export default function App() {
-  let name = 'ram'
+      let name = 'ram'
   return (
 
     <View style={{marginTop:90}}>
       <Text>{name}</Text>
       <Button title='Click Me'></Button>
+      <Student1/>
+      <Text></Text>
+      <Student2/>
+      <Text></Text>
+      <Student3/>
+      <Text></Text>
+      <CompanyData/>
+
     </View>
   );
 }
 
+const Student1 = ()=>{
+  return (
+    <View>
+      <Text>Name : Pushit Chaudhary</Text>
+      <Text>Address : Lahan</Text>
+      <Text>Education: Bsc Hons Computing</Text>
+    </View>
+  )
+}
 
+
+
+const Student2 = ()=>{
+  return (
+    <View>
+      <Text>Name : Anish</Text>
+      <Text>Address : Damauli</Text>
+    </View>
+  )
+}
+
+const Student3 = ()=>{
+  return(
+    <View>
+      <Text>Name : Yubraj</Text>
+      <Text>Address : Pokhara</Text>
+    </View>
+  )
+}
---
+++ components/CompanyData.js
@@ -0,0 +1,14 @@
+import {Text,View} from 'react-native'
+
+const CompanyData = ()=>{
+    return(
+        <View>
+            <Text>Company Name : XYZ.company </Text>
+            <Text>Address : Kathmandu</Text>
+            <Text>Contact : 9345345</Text>
+        </View>
+    )
+}
+
+
+export default CompanyData;
