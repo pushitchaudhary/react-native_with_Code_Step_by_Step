741a47b1df493b0e7ef9375e012c9b2a89ea2986
pushitchaudhary <pushitcoc@gmail.com>
2024-04-06 22:35:05 +0545

    flatlist

--- App.js
+++ App.js
@@ -1,36 +1,60 @@
 import React, { useState } from "react";
 
-import { View,Text, Button, TextInput, StyleSheet  } from "react-native";
+import { View,Text, Button, TextInput, StyleSheet , FlatList } from "react-native";
 
 import styles from "./css/styles";
 
 
 export default function App() {
-  const [name, setName] = useState('')
-  const [password, setPassword] = useState('')
-  const [address, setAddress] = useState('')
-  const [contact, setContact] = useState('')
-  const [email, setEmail] = useState('')
-  const [display, setDisplay] = useState(false)
+
+  const UserData = [
+    {
+      id : 1,
+      name : "pushit",
+      age : 23
+    },
+    {
+      id : 2,
+      name : "ram",
+      age : 19
+    },
+    {
+      id : 3,
+      name : "om",
+      age : 11
+    }
+  ]
+
+  // const [name, setName] = useState('')
+  // const [password, setPassword] = useState('')
+  // const [address, setAddress] = useState('')
+  // const [contact, setContact] = useState('')
+  // const [email, setEmail] = useState('')
+  // const [display, setDisplay] = useState(false)
 
 
-  
   return (
   <View style={{marginTop:90}}>
-    <Text style={styless.inputText}>Enter Name</Text>
+    
+    {/* <Text style={styless.inputText}>Enter Name</Text>
     <TextInput style={styless.inputBox} value={name} onChangeText={(text)=>setName(text)}  placeholder="Enter Your Name"/>
+    
     <Text style={styless.inputText}>Password</Text>
     <TextInput style={styless.inputBox} value={password} secureTextEntry={true} onChangeText={(text)=>setPassword(text)}  placeholder="Password" />
+   
+
     <Text style={styless.inputText}> Address </Text>
     <TextInput style={styless.inputBox} value={address} onChangeText={(text)=>setAddress(text)} placeholder="Address"/>
+    
     <Text style={styless.inputText}>Contact Number</Text>
     <TextInput style={styless.inputBox} value={contact} onChangeText={(text)=>setContact(text)} placeholder="Contact Number" />
+    
     <Text style={styless.inputText}>Email</Text>
     <TextInput style={styless.inputBox} value={email} onChangeText={(text)=>setEmail(text)} placeholder="email"/>
+   
     <Button title="Print Details" onPress={()=>setDisplay(true)} style={styless.submitBtn}></Button>
     <Button title="Clear field" onPress={()=>{setDisplay(false),setName(''),setAddress(''),setPassword(''),setEmail(''),setContact('') }}></Button>
   
-    <Text></Text>
     <Text></Text>
     <Text></Text>
 
@@ -43,7 +67,15 @@ export default function App() {
         <Text>Contact : {contact}</Text>
         <Text>Email : {email}</Text>
       </View> : null
-    }
+    } */}
+
+    <Text style={styless.textStyle}>Id Name Age</Text>
+   <FlatList
+    data = {UserData}
+    renderItem ={({item})=> <Text style={styless.textStyle}>{item.id} {item.name} {item.age}</Text>}
+    keyExtractor={item=>item.id}
+
+    />
   
   </View>  
   );
@@ -63,7 +95,7 @@ const styless = StyleSheet.create({
   },
 
   inputText : {
-    fontSize : 20,
+    fontSize : 40,
     marginLeft: 10,
     marginBottom : 5
     
@@ -71,6 +103,15 @@ const styless = StyleSheet.create({
   submitBtn : {
     color : 'red',
     fontSize : 20
+  }
+  ,
+
+  textStyle : {
+    fontSize : 20,
+    color : 'red',
+    backgroundColor : 'yellow',
+    margin : 10,
+    height : 40
   }
 })
 
