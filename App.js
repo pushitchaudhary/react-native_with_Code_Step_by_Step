02aab8a0115ef6fbcdbe1fcaaeb22f03e82b5363
pushitchaudhary <pushitcoc@gmail.com>
2024-04-06 15:57:19 +0545

    react native -view text butto

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-import { View,Text } from "react-native";
+import { View,Text, Button } from "react-native";
 
 export default function App() {
+  let name = 'ram'
   return (
-    <View>
-      <Text style={{fontSize:40, color:'red'}}>Hello this</Text>
-      <Text style={{color: 'red'}}>This is Nepal</Text>
+
+    <View style={{marginTop:90}}>
+      <Text>{name}</Text>
+      <Button title='Click Me'></Button>
     </View>
   );
 }
