029400cdaedd41d9947e4132fa53f2268b0f0986
pushitchaudhary <pushitcoc@gmail.com>
2024-04-06 19:42:04 +0545

    css - inline , internal , external

--- App.js
+++ App.js
@@ -1,37 +1,18 @@
 import React, { useState } from "react";
 
-import { View,Text, Button } from "react-native";
-import CompanyData from "./components/CompanyData";
+import { View,Text, Button  } from "react-native";
+
+import styles from "./css/styles";
+
 
 export default function App() {
-  const [name , setName] = useState('Pushit')
-
-  const carName = (value)=>{
-    setName(value)
-  }
 
   return (
-
-    <View style={{marginTop:90}}>
-     <Text>{name}</Text>
-
-    <Button title="click me" onPress={()=>carName('k xha')}></Button>
-  
-
-  <UserData name="pushit" age="23"/>
-    </View> 
-    
+  <View style={{marginTop:90}}>
+    <Text style={styles.textBox}>My Name Is Pushit</Text>
+    <Text style={styles.location}>I am from Lahan</Text>
+  </View>  
   );
 }
 
 
-
-const UserData = (props)=>{
-
-  return (
-    <View>
-      <Text>Name :{props.name} </Text>
-      <Text>Age : {props.age}</Text>
-    </View>
-  )
-}
---
+++ css/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+    textBox : {
+      color : 'red',
+      backgroundColor : 'pink',
+      fontSize : 40
+    },
+    location : {
+      color : 'green',
+      backgroundColor : 'yellow',
+      height : 100,
+      textAlignVertical : 'center',
+      textAlign : 'center'
+  
+    }
+  })
+  
+
+  export default styles
+  
