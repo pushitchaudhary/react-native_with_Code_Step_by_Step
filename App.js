cc72e5b510972134db6d49deffff6d1122a25035
pushitchaudhary <pushitcoc@gmail.com>
2024-04-09 22:20:31 +0545

    responsive layout

--- App.js
+++ App.js
@@ -1,26 +1,56 @@
 import React, { useState , Component, useEffect} from "react";
 
 import { View,Text, Button, TextInput, StyleSheet, ScrollView, FlatList  } from "react-native";
-import Student from "./components/Student";
+
 
 const App = ()=>{
-
-  const [dispaly, setDisplay] = useState(true)
-
-
   return(
-    <View style={{marginTop:90}}>
-      <Button title="Troggle Compoenent" onPress={()=>setDisplay(!dispaly)}></Button>
-
-      {
-        dispaly ?  <Student/> : null
-      }
-     
+    <View style={style.main}>
+      <View style={style.box1}>
+        <View style={style.col1}></View>
+        <View style={style.col2}></View>
+        <View style={style.col3}></View>
+      </View>
+      <View style={style.box2}></View>
+      <View style={style.box3}></View>
     </View>
-
   )
 
 }
+
+const style = StyleSheet.create({
+  main : {
+    backgroundColor : 'red',
+    flex : 1
+  },
+  box1 : {
+    flex : 1,
+    backgroundColor : 'yellow',
+    flexDirection : 'row'
+  },
+  box2 : {
+    backgroundColor : 'green',
+    flex : 1,
+  },
+  box3 : {
+    backgroundColor : 'brown',
+    flex : 1
+  },
+
+  col1 : {
+    backgroundColor : 'red',
+    flex : 1
+  },
+  col2 : {
+    backgroundColor : 'blue',
+    flex : 1
+  },
+  col3 : {
+    backgroundColor : 'white',
+    flex : 1
+  }
+})
+
 
 export default App
 
