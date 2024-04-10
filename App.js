4775fed18af0dd5f113e14d910ab0c4afab8bb08
pushitchaudhary <pushitcoc@gmail.com>
2024-04-10 23:40:20 +0545

    loader

--- App.js
+++ App.js
@@ -1,52 +1,34 @@
 import React, { useState , Component, useEffect} from "react";
 
-import { View,Text, StyleSheet, TouchableHighlight , ScrollView, TouchableOpacity} from "react-native";
+import { View,Text, StyleSheet, Button ,ActivityIndicator, TouchableHighlight, TouchableOpacity } from "react-native";
 
 
 const App = ()=>{
 
-  const Skill = [
-    {
-      id : 1,
-      name : "Node js"
-    },
-    {
-      id : 2,
-      name : "PHP"
-    },
-    {
-      id : 3,
-      name : "JAVA"
-    },
-    {
-      id : 4,
-      name : "Python"
-    }
-  ]
+  const [display, setDisplay] = useState(false)
 
+  const runFun = ()=>{
+    setDisplay(true)
 
-
-
-  const [checked, setChecked] = useState(1)
-
+    setTimeout(()=>{
+      setDisplay(false)
+    },3000)
+  }
 
   return(
     <View style={style.mainBox}>
 
+      {/* Method 1 */}
+      {/* <ActivityIndicator size={'large'} animating={display} color={'red'} /> */}
+
+      {/* Method 2 */}
       {
-        Skill.map((item)=> <TouchableOpacity key={item.id} onPress={()=>setChecked(item.id)}>
-        <View style={{flexDirection: 'row'}}>
-          <View style={style.radio}>
-            {
-              checked == item.id ?  <View style={style.radionBg}></View> : null
-            }
-          </View>
-          <Text style={style.btntext}>{item.name}</Text>
-        </View>
-        <Text></Text>
-      </TouchableOpacity>)
+        display ?  <ActivityIndicator size={'large'} animating={display} color={'green'} /> : null
       }
 
+      <Button title='loader' onPress={()=>runFun()} />
+
+    
     </View>
   )
 
@@ -60,23 +42,12 @@ const style = StyleSheet.create({
     alignItems : 'center',
     justifyContent : 'center'
   },
-  btntext : {
-    fontSize : 25,
-    margin : 5
-  },
-  radio : {
-    height : 40,
-    width : 40,
-    borderWidth : 2,
-    borderRadius : 20
-  },
-  radionBg : {
-    height : 26,
-    backgroundColor : 'black',
-    width : 26,
-    borderRadius : 12,
-    margin : 5
+
+  text : {
+    fontSize : 20,
+    marginTop : 30
   }
+ 
 })
 
 
