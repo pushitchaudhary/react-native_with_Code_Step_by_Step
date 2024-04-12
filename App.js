2d080400e0f7f0315ebb7230cd05589a2003891b
pushitchaudhary <pushitcoc@gmail.com>
2024-04-12 15:26:50 +0545

    webview

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { useState , Component, useEffect} from "react";
 
-import { View,Text, StyleSheet, StatusBar, Platform} from "react-native";
+import { View,Text, StyleSheet, StatusBar, Platform, } from "react-native";
+
+import {WebView} from 'react-native-webview'
 
 
 const App = ()=>{
@@ -8,11 +10,10 @@ const App = ()=>{
   const [show, setShow] = useState(false)
 
   return(
-    <View style={style.mainBox}>
 
-     <Text>{Platform.OS}</Text>
 
-    </View>
+      <WebView style={{marginTop: 60}} source={{uri:"https://www.youtube.com/watch?v=4vVf18LXlOo&list=PL8p2I9GklV468O2wk-n8Q1KmtMhnHHj4C&index=37"}} />
+ 
   )
 
 }
